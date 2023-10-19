13f8bc04e568001536d67e945ac6288da67dc0d0
BaBuJloH <BaBuJloH1993@gmail.com>
2023-10-19 04:27:37 +0300

    исправлен баг POST и GET запросов, подключен pgxpool

--- model/order/order.go
+++ model/order/order.go
@@ -4,11 +4,11 @@ import "time"
 
 // Data - заказ
 type Data struct {
-	OrderUID    *string   `json:"order_uid"`
-	TrackNumber *string   `json:"track_number"`
-	Entry       *string   `json:"entry"`
-	Delivery    *Delivery `json:"delivery"`
-	Payment     *struct {
+	OrderUID    string   `json:"order_uid"`
+	TrackNumber string   `json:"track_number"`
+	Entry       string   `json:"entry"`
+	Delivery    Delivery `json:"delivery"`
+	Payment     struct {
 		Transaction  string `json:"transaction"`
 		RequestID    string `json:"request_id"`
 		Currency     string `json:"currency"`
@@ -20,15 +20,15 @@ type Data struct {
 		GoodsTotal   int    `json:"goods_total"`
 		CustomFee    int    `json:"custom_fee"`
 	} `json:"payment"`
-	Items             *[]ChrtItem `json:"items"`
-	Locale            *string     `json:"locale"`
-	InternalSignature *string     `json:"internal_signature"`
-	CustomerID        *string     `json:"customer_id"`
-	DeliveryService   *string     `json:"delivery_service"`
-	Shardkey          *string     `json:"shardkey"`
-	SmID              *int        `json:"sm_id"`
-	DateCreated       *time.Time  `json:"date_created"`
-	OofShard          *string     `json:"oof_shard"`
+	Items             []ChrtItem `json:"items"`
+	Locale            string     `json:"locale"`
+	InternalSignature string     `json:"internal_signature"`
+	CustomerID        string     `json:"customer_id"`
+	DeliveryService   string     `json:"delivery_service"`
+	Shardkey          string     `json:"shardkey"`
+	SmID              int        `json:"sm_id"`
+	DateCreated       time.Time  `json:"date_created"`
+	OofShard          string     `json:"oof_shard"`
 }
 
 type Delivery struct {
--- repositories/orders_repo/repo.go
+++ repositories/orders_repo/repo.go
@@ -10,7 +10,7 @@ import (
 	"log"
 	"time"
 
-	//"github.com/jackc/pgx/v5"
+	
 )
 
 const TableName = "orders"
